a025771d2c43df01373a124318a0003a69fda207
martin <martudev@gmail.com>
2023-12-30 00:04:57 -0300

    cards

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom"
 import "./Item.css"
+import { useState } from "react"
 const Item = ({producto}) => {
+
+    const [zoom, setZoom] = useState (false); 
+    
+    const handleMouseOver = () => {
+        setZoom(true)
+    };
+
+    const handleMouseLeave = () =>{
+        setZoom(false)
+    };
+
+    const estiloCard = {
+        transform: zoom ? 'scale(1.1)' : 'scale(1)',
+        transition: 'transform 0.3 ease-in-out'
+    };
+
     return (
-        <div className="card expand-lg">
+        <div 
+        style={estiloCard}
+        onMouseOver={handleMouseOver} 
+        onMouseLeave={handleMouseLeave}
+        className="card expand-lg"
+        >
             <img className="image" src={producto.imagen} alt="" />
             <p className="carditem"> { producto.nombre } </p>
             <p className="carditem">{producto.descripcion}</p>
